eef7aa995b5b00229be782860105ed3106d205f0
Stan Dyck <stan.dyck@gmail.com>
2012-11-05 10:24:51 -0800

    Moved sample people out of core. Updated to clojure 1.4

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject secret-santa "1.0.0-SNAPSHOT"
   :description "Given a list of people select a secret santa for each with rules detailed in the readme"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
--- src/secret_santa/core.clj
+++ src/secret_santa/core.clj
@@ -1,37 +1,7 @@
 (ns secret-santa.core
-  (:use [secret-santa.rules]))
+  (:use [secret-santa.people]
+        [secret-santa.rules]))
 
-(def *sample-people*
-  [{:name "Stan"     :email "stan.dyck@gmail"      :family 0}
-   {:name "Laura"    :email "laura@example.com"    :family 0}
-   {:name "George"   :email "george@example.com"   :family 1}
-   {:name "Edna"     :email "edna@example.com"     :family 1}
-   {:name "Brian"    :email "brian@example.com"    :family 2}
-   {:name "Lynell"   :email "lynell@example.com"   :family 2}
-   {:name "Sam"      :email "sam@example.com"      :family 2}
-   {:name "Marcus"   :email "marcus@example.com"   :family 2}
-   {:name "Janine"   :email "janine@example.com"   :family 3}
-   {:name "Matt"     :email "matt@examplee.com"    :family 3}
-   {:name "ChaeYoon" :email "chaeyoon@example.com" :family 3}
-   {:name "Jon"      :email "jon@example.com"      :family 4}
-   {:name "Lisa"     :email "lisa@example.com"     :family 4}
-   {:name "Simon"    :email "simon@example.com"    :family 4}
-   {:name "Seth"     :email "seth@example.com"     :family 4}])
 
-(def *sample-last-recipients*
-  {:Stan     :George
-   :Laura    :Seth
-   :George   :Lisa
-   :Edna     :Janine
-   :Brian    :Simon
-   :Lynell   :Matt
-   :Sam      :Laura
-   :Marcus   :Jon
-   :Janine   :Marcus
-   :Matt     :Edna
-   :ChaeYoon :Lynell
-   :Jon      :Brian
-   :Lisa     :Stan
-   :Simon    :ChaeYoon
-   :Seth     :Sam})
-
+(defn -main []
+  (println "Hello, " sample-people))
